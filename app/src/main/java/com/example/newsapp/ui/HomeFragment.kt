a5e1ce4865850a442f94c720176cbc8aa9f145f1
Suhail <suhailabbas23@gmail.com>
2023-06-17 17:54:30 +0530

    Code optimized

--- app/src/main/java/com/example/newsapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/HomeFragment.kt
@@ -5,33 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.newsapp.R
 import com.example.newsapp.adapter.NewsAdapter
-import com.example.newsapp.data.newslist_response.Article
 import com.example.newsapp.databinding.FragmentHomeBinding
 import com.example.newsapp.viewModels.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private val viewModel: HomeViewModel by viewModels()
-
     private var _binding: FragmentHomeBinding? = null
-    private var onItemClickListener: ((Article) -> Unit)? = null
-
-
     private val binding get() = _binding
     private lateinit var newsAdapter: NewsAdapter
 
@@ -40,8 +26,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        newsAdapter = NewsAdapter()
+        initUI()
         getNews()
+        return binding?.root
+    }
+
+    /**
+     * Initializing the UI
+     */
+    private fun initUI() {
+        newsAdapter = NewsAdapter()
         binding?.newsRecyclerView?.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(context)
@@ -50,21 +44,19 @@ class HomeFragment : Fragment() {
             val action = HomeFragmentDirections.actionHomeFragmentToDetailedFragment(it)
             findNavController().navigate(action)
         }
-
         binding?.swiperefresh?.setOnRefreshListener {
-           viewModel.refreshNews()
+            viewModel.refreshNews()
         }
-
-        return binding?.root
     }
 
+    /**
+     * Method to get the latest news from API
+     */
     private fun getNews() {
-
         viewModel.newsFlow.observe(viewLifecycleOwner) {
             newsAdapter.differ.submitList(it)
             binding?.swiperefresh?.isRefreshing = false
         }
-
     }
 
     override fun onDestroyView() {
